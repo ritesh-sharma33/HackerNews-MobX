1165569ada5838f35344b570f0a074c0265b90fe
Ritesh Sharma <sharmaritesh3312@gmail.com>
2020-05-16 20:02:55 +0530

    API calling with MobX done.

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:hacker_news_mobx/news_widgets.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HackerNewsExample(),
+      home: HackerNewsExample()
     );
   }
 }
